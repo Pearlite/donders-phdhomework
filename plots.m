38d540187703194f7253600b32b5c392ab32cde8
Marrit Zuure <marrit@gmail.com>
2015-10-02 15:31:56 +0200

    Edited plots file to produce graphs plotting firing rate as a function of inputs under multiple circumstances.

--- plots.m
+++ plots.m
@@ -5,14 +5,14 @@
 
 %%%%%GLOBAL PARAMETER DEFINITIONS%%%%%
 % timesteps = number of population activation update cycles. Takes integer.
-timesteps = 200;
+timesteps = 100;
 
 % iE, iI = external input to E and I populations of net A. Takes double (for
 % constant input) or vector of size timesteps (for varying input).
 iE = zeros(1, timesteps);
-iE(10:100) = 5;
+iE(21:50) = 10;
 iI = zeros(1, timesteps);
-iI(10:100) = 5;
+iI(21:50) = 10;
 
 % wEE, wEI, wIE, wII = weights representing connection strength between
 % populations. wEE{1} and equivalent is used for net A, wEE{2} and equivalent
@@ -47,50 +47,94 @@ typeI = 'sigmoid';
 %%
 %%%%%PLOT 1%%%%%%
 % Replication of plots from Figure 3 in Jadi & Sejnowski (2014) as proof of
-% principle.
+% principle. Only doing this for network A, network B is left out.
 
-%fewer timesteps to improve simulation speed
-timesteps = 40;
+clear nodeE;
+clear nodeI;
 
 counter = 0;
 
-for iE = 0:0.5:10
-    for iI = 0:0.5:10
+for iE = 0:0.2:20
+    for iI = 0:0.2:20
         counter = counter+1;
         [nodeE_temp, nodeI_temp] = RunNetwork ( timesteps, iE, iI, wEE, wEI, wIE, wII, wAB, ...
             slopeE, slopeI, thresholdE, thresholdI, typeE, typeI );
+            nodeE{1}.iE(counter) = iE;
+            nodeE{1}.iI(counter) = iI;
+            nodeE{1}.firingrate(counter, 1:numel(nodeE_temp{1})) = nodeE_temp{1};
+            
+            nodeI{1}.iE(counter) = iE;
+            nodeI{1}.iI(counter) = iI;
+            nodeI{1}.firingrate(counter, 1:numel(nodeI_temp{1})) = nodeI_temp{1};
+    end
+end
+
+%convert nodeE and nodeI results to mean firing rate over a period of 
+%time where network is presumed to be stable (51 to 100 timesteps)
+firingrateE = reshape(mean(nodeE{1}.firingrate(:, 51:100),2), numel(0:0.2:20), numel(0:0.2:20));
+figure;
+pcolor(firingrateE);
+
+firingrateI = reshape(mean(nodeI{1}.firingrate(:, 51:100),2), numel(0:0.2:20), numel(0:0.2:20));
+figure;
+pcolor(firingrateI);
+
+%Note: the output here seems to differ from Jadi & Sejnowski figure 3b.
+%Add axes + colorbar + title
+
+%opt: plot same for power, frequency
+
+
+%%
+%%%%%PLOT 2%%%%%%
+% Brief input to net A simulated for a duration of t=30 (from timestep 21
+% to 50).
+% Input strength iE varied from 0 to 20.
+% Input strength iI varied from 0 to 20.
+
+clear nodeE;
+clear nodeI;
+
+counter = 0;
+
+iE = zeros(1, timesteps);
+iI = zeros(1, timesteps);
+
+for k = 1:0.2:21
+    iE(21:50) = k-1;
+    for l = 1:0.2:21
+        iI(21:50) = l-1;
+        counter = counter + 1;
+        [nodeE_temp, nodeI_temp] = RunNetwork ( timesteps, iE, iI, wEE, wEI, wIE, wII, wAB, ...
+            slopeE, slopeI, thresholdE, thresholdI, typeE, typeI );
         for i = 1:2
-            nodeE{i}.iE(counter) = iE;
-            nodeE{i}.iI(counter) = iI;
+            nodeE{i}.iE(counter) = iE(22);
+            nodeE{i}.iI(counter) = iI(22);
             nodeE{i}.firingrate(counter, 1:numel(nodeE_temp{i})) = nodeE_temp{i};
             
-            nodeI{i}.iE(counter) = iE;
-            nodeI{i}.iI(counter) = iI;
+            nodeI{i}.iE(counter) = iE(22);
+            nodeI{i}.iI(counter) = iI(22);
             nodeI{i}.firingrate(counter, 1:numel(nodeI_temp{i})) = nodeI_temp{i};
         end
     end
 end
 
-%surf(nodeE{1}.iE, nodeE{1}.iI, nodeE{1}.firingrate');
+firingrateEA = reshape(mean(nodeE{1}.firingrate,2), numel(1:0.2:21), numel(1:0.2:21));
+figure;
+pcolor(firingrateEA);
 
-%%
-%%%%%PLOT 2%%%%%%
-% Brief input to net A simulated for a duration of t=80. -> woops, I don't
-% actually do this, I simulate input for the entire duration because I set
-% iI and iE to a single value instead of a vector.
-% Input strength iE varied from 0 to 20
-% Input strength iI varied from 0 to 20
+firingrateIA = reshape(mean(nodeI{1}.firingrate,2), numel(1:0.2:21), numel(1:0.2:21));
+figure;
+pcolor(firingrateIA);
 
-% for iE = 1:20
-%     for iI = 1:20
-%         [nodeE_temp, nodeI_temp] = RunNetwork ( timesteps, iE, iI, wEE, wEI, wIE, wII, wAB, ...
-%             slopeE, slopeI, thresholdE, thresholdI, typeE, typeI );
-%         for i = 1:2
-%             nodeE{i}(iE, iI, :) = nodeE_temp{i};
-%             nodeI{i}(iE, iI, :) = nodeI_temp{i};
-%         end
-%     end
-% end
+firingrateEB = reshape(mean(nodeE{2}.firingrate,2), numel(1:0.2:21), numel(1:0.2:21));
+figure;
+pcolor(firingrateEB);
+
+firingrateIB = reshape(mean(nodeI{2}.firingrate,2), numel(1:0.2:21), numel(1:0.2:21));
+figure;
+pcolor(firingrateIB);
+
 % 
 % %quick tester
 % plot(squeeze(nodeE{1}(1,1,:))) %matrix needs to be squeezed to remove singleton dimension
@@ -101,36 +145,39 @@ end
 % %values of iI, gives a nice impression of when a network oscillates and
 % %when it doesn't
 
-
-
-
-% question 1: which range should certain inputs/weights take?
-% question 2: how do I characterize the outputs (activation?) FFT on
-% nodeE{1}?
-% question 3: how do I plot synchrony between populations? -> check how
-% Mike does this. Plot spectral coherence?
+%mscohere(x,y,window)
 
 %%
 %%%%%PLOT 2%%%%%
-%network A-B connectivity varied
-%
+% Brief input to net A simulated for a duration of t=30 (from timestep 21
+% to 50).
+% Input strength iE set to 10 (default).
+% Input strength iI set to 10 (default).
+% Connectivity strength between Networks A and B varied from 0 to 20.
+
+clear nodeE;
+clear nodeI;
+
+counter = 0;
+
+for k = 1:0.2:21
+    wAB = k-1;
+    counter = counter + 1;
+    [nodeE_temp, nodeI_temp] = RunNetwork ( timesteps, iE, iI, wEE, wEI, wIE, wII, wAB, ...
+        slopeE, slopeI, thresholdE, thresholdI, typeE, typeI );
+    for i = 1:2
+        nodeE{i}.wAB(counter) = wAB;
+        nodeE{i}.firingrate(counter, 1:numel(nodeE_temp{i})) = nodeE_temp{i};
+        
+        nodeI{i}.wAB(counter) = wAB;
+        nodeI{i}.firingrate(counter, 1:numel(nodeI_temp{i})) = nodeI_temp{i};
+    end
+end
+
+%add plots
+
 
 %%
 %%%%%PLOT 3%%%%%
 %input duration varied
 %
-
-%%
-%%%%%PLOT 4%%%%%
-%input strength and duration varied
-%
-
-%%
-%%%%%PLOT 5%%%%%
-%input strength and network connectivity varied
-%
-
-
-%%%%%SIMULATE NETWORK%%%%%
-[nodeE, nodeI] = RunNetwork ( timesteps, iE, iI, wEE, wEI, wIE, wII, wAB, ...
-    slopeE, slopeI, thresholdE, thresholdI, typeE, typeI );
