701782088ad47c41ce64ff134613a85182e2a138
Marrit Zuure <marrit@gmail.com>
2015-10-02 21:28:05 +0200

    Clarified and simplified part of the generated plots.

--- plots.m
+++ plots.m
@@ -7,7 +7,7 @@
 % timesteps = number of population activation update cycles. Takes integer.
 timesteps = 100;
 
-% iE, iI = external input to E and I populations of net A. Takes double (for
+% iE, iI = external input to E and I populations of net A. Takes a double (for
 % constant input) or vector of size timesteps (for varying input).
 iE = zeros(1, timesteps);
 iE(21:50) = 10;
@@ -15,7 +15,8 @@ iI = zeros(1, timesteps);
 iI(21:50) = 10;
 
 % wEE, wEI, wIE, wII = weights representing connection strength between
-% populations. wEE{1} and equivalent is used for net A, wEE{2} and equivalent
+% populations. wEE{1} and equivalent is used for net A, wEE{2} and
+% equivalent
 % is used for net B. If only one value is given this is used for both
 % net A and B. Takes doubles (for constant weights) or vectors of size
 % timesteps (for varying weights).
@@ -25,17 +26,17 @@ wIE = 20; %default = 20
 wII = 1; %default = 1
 
 % wAB = weight representing unidirectional connection strength between
-% nets A and B. Takes double (for constant weight) or vector of size
+% nets A and B. Takes a double (for constant weight) or vector of size
 % timesteps (for varying weight).
 wAB = 3;
 
 % slopeE, slopeI = rate at which E and I population responses change as
-% function of input. Takes double.
+% function of input. Takes a double.
 slopeE = 1; %default for sigmoid = 1, default for linear = 0.25
 slopeI = 1; %default for sigmoid = 1, default for powerlaw = 0.005
 
 % thresholdE, thresholdI = net input below which response function value is
-% 0. Takes double.
+% 0. Takes a double.
 thresholdE = 5; %default for sigmoid = 5, default for linear = 1
 thresholdI = 20; %default for sigmoid = 20, default for powerlaw = 12
 
@@ -47,11 +48,8 @@ typeI = 'sigmoid';
 %%
 %%%%%PLOT 1%%%%%%
 % Replication of plots from Figure 3 in Jadi & Sejnowski (2014) as proof of
-% principle. Only doing this for network A, network B is left out.
-
-clear nodeE;
-clear nodeI;
-
+% principle. The graphs are only generated for network A; network B data is
+% left out.
 counter = 0;
 
 for iE = 0:0.2:20
@@ -79,11 +77,10 @@ firingrateI = reshape(mean(nodeI{1}.firingrate(:, 51:100),2), numel(0:0.2:20), n
 figure;
 pcolor(firingrateI);
 
-%Note: the output here seems to differ from Jadi & Sejnowski figure 3b.
-%Add axes + colorbar + title
+clear;
 
-%opt: plot same for power, frequency
-
+%Note: the output here seems to differ from Jadi & Sejnowski figure 3b,
+%most likely on account of different input scaling.
 
 %%
 %%%%%PLOT 2%%%%%%
@@ -91,9 +88,16 @@ pcolor(firingrateI);
 % to 50).
 % Input strength iE varied from 0 to 20.
 % Input strength iI varied from 0 to 20.
-
-clear nodeE;
-clear nodeI;
+% The four plots show mean firing rate for net A (node E and I) and net B 
+%(node E and I) as a function of the input values. The net A graphs are
+% nearly identical to the proof-of-principle graphs from the previous
+% section. This suggests that taking the mean of the entire timeseries given
+% brief input (done in this section) is nearly indistinguishable from taking 
+% the mean over the stable-firing part of the timeseries given constant
+% input (done in the previous section). This knowledge allows us to simplify 
+% our analyses, taking one general mean (and thus condensing a timeseries 
+% into a single value, facilitating plotting) rather than arbitrarily 
+% defining a "valid" range within the timeseries.
 
 counter = 0;
 
@@ -135,28 +139,15 @@ firingrateIB = reshape(mean(nodeI{2}.firingrate,2), numel(1:0.2:21), numel(1:0.2
 figure;
 pcolor(firingrateIB);
 
-% 
-% %quick tester
-% plot(squeeze(nodeE{1}(1,1,:))) %matrix needs to be squeezed to remove singleton dimension
-% %network oscillates at iE = 10 and iI = 10, but not at iE = 1 and iI = 1 or
-% %iI = 20. Nice!
-% 
-% %pcolor(squeeze(nodeE{1}(:,10,:))) %plot this a few times for different
-% %values of iI, gives a nice impression of when a network oscillates and
-% %when it doesn't
-
-%mscohere(x,y,window)
+clear;
 
 %%
-%%%%%PLOT 2%%%%%
+%%%%%PLOT 3%%%%%
 % Brief input to net A simulated for a duration of t=30 (from timestep 21
 % to 50).
 % Input strength iE set to 10 (default).
 % Input strength iI set to 10 (default).
 % Connectivity strength between Networks A and B varied from 0 to 20.
-
-clear nodeE;
-clear nodeI;
 
 counter = 0;
 
@@ -174,10 +165,27 @@ for k = 1:0.2:21
     end
 end
 
-%add plots
+%plot average firing rate over entire duration against A-B connection
+%weight
+figure;
+plot(nodeE{2}.wAB, mean(nodeE{2}.firingrate,2));
 
+figure;
+plot(nodeI{2}.wAB, mean(nodeI{2}.firingrate,2));
+
+
+figure;
+plot(nodeE{2}.wAB, nodeE{2}.firingrate);
+
+figure;
+plot(nodeI{2}.wAB, mean(nodeI{2}.firingrate,2));
+
+%cross-correlation between signals
+a = xcorr(nodeE{1}.firingrate(30,:), nodeE{2}.firingrate(30,:));
+
+clear;
 
 %%
-%%%%%PLOT 3%%%%%
+%%%%%PLOT 4%%%%%
 %input duration varied
 %
